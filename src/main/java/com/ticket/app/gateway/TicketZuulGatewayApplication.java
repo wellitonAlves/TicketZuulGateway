49dc6fd9fe2804295b4cabefe5ce73aa86beb176
jose <josealves_w@yahoo.com.br>
2020-04-20 23:01:38 -0300

    first commit

---
+++ src/main/java/com/ticket/app/gateway/TicketZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketZuulGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/app/gateway/TicketZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
